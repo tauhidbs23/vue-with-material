9f0756a8cede8a2228e54aa4f43d97108ec328ac
tauhidbs23 <tauhidul.islam@brainstation-23.com>
2021-09-22 18:31:27 +0600

    vue-material added

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Post from "../components/Post.vue";
+import Category from "../components/Category.vue";
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Post',
+    component: Post
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/category',
+    name: 'Category',
+    component: Category
   }
 ]
 
